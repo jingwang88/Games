bd6235361eef5722e7493038587e40065f6cf63e
jingwang88 <wj154419@163.com>
2016-04-08 14:49:54 +0800

    限定游戏范围，修复几处流程bug

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ window.onload = function () {
 	var getTarget = function (event) {
 		return event.target || event.srcElement;
 	}
+	var getEvent = function (e) {
+		return e || window.event;
+	}
 	function addEventHandler (ele, type, handler) {
 		if (ele.addEventListener) {
 			ele.addEventListener(type, handler);
@@ -78,13 +81,15 @@ window.onload = function () {
 			
 		}
 	});
+
+	 
 	// 获取坐标
-	function getPosition (li) {
+	function getPosition (object) {
 		var left = 0, top=0;
-		while (li) {
-			left += li.offsetLeft;
-			top += li.offsetTop;
-			li = li.offsetParent;
+		while (object) {
+			left += object.offsetLeft;
+			top += object.offsetTop;
+			object = object.offsetParent;
 		}
 		return {x: left, y: top};
 	}
@@ -134,7 +139,7 @@ window.onload = function () {
 		ele.style.top = "";
 		ele.style.zIndex = "";
 	}
-	// 判断是否成功
+	// 判断是否拼图成功
 	function isSuccess(index) {
 		var images = document.getElementsByClassName("img-block");
 		for (var i=1; i<=images.length; i++) {
@@ -145,53 +150,84 @@ window.onload = function () {
 		}
 		return true;
 	}
+	// 判断是否超界 
+	function isInGamezone (relaPosX, relaPosY) {
+		if (relaPosX>=70 && relaPosY>=40 && relaPosX<=640 && relaPosY<=370 ) {
+			return true;
+		} else {
+			return false;
+		}
 
-
+	}
+	
+	// 游戏区容器的坐标
+	
 	btn.onclick = function () {
 		// 第index张游戏图片
 		var index = getIndex();
 		var randArr = randomSort(25);
 		var ul = geneRandomPicsList(randArr, index);
+	
 		gameZone.innerHTML = "";
 		gameZone.appendChild(ul);
-
-		// 获取每一项
-		
+		// 阻止拖拽图片在新窗口打开
+		for(i in document.images) document.images[i].ondragstart = function() {return false}; 
 
 		var handleMousedown = function (e) {
 			// while (getTarget(e).tagName !== "IMG");
+			console.log("mousedown");
 			var srcEle = getTarget(e);
 			// 鼠标与容器的间隙
 			var offsetX = e.offsetX;
 			var offsetY = e.offsetY;
-			srcEle.style.zIndex = 3;
 			var position = getPosition(srcEle);
 			var temp = 0;
-			var relaPosX = 0;
-			var relaPosY = 0;
+			var mouX = e.pageX;
+			var mouY = e.pageY;
+			var gameZonePos = getPosition(gameZone);
+			var relaPosX = mouX - offsetX - gameZonePos.x + 71;
+			var relaPosY = mouY - offsetY - gameZonePos.y + 41;
 			// 判断是否触发mousemove的bug
 			var moveFlag = false;
-			var gameZonePos = getPosition(gameZone);
+			// 
 			var li = [];
 			var image = [];
+			srcEle.style.zIndex = 3;
+			// 获取每一项
 			for (var i=0; i<randArr.length; i++) {
 				li[i] = document.getElementsByClassName("list")[i];
 				image[i] = document.getElementsByClassName("img-block")[i];
 			}
+			// mousmove
+			var dx = 0;
+			var dy = 0;
 			var handleMousemove = function(eve) {
+				// if (isInGamezone(eve, gameZonePos)) {
+				console.log("mousemove");
 				var target = getTarget(eve);
 				var mouseX = eve.pageX;
 				var mouseY = eve.pageY;
-				var dx = mouseX - position.x - offsetX;
-				var dy = mouseY - position.y - offsetY;
+				if (isInGamezone(relaPosX, relaPosY)) {
+					dx = mouseX - position.x - offsetX;
+					dy = mouseY - position.y - offsetY;
+					relaPosX = mouseX - offsetX - gameZonePos.x + 71;
+					relaPosY = mouseY - offsetY - gameZonePos.y + 41;
+					var num = (Math.ceil(relaPosY/82)-1)*5 + Math.floor(relaPosX/142);
+					moveFlag = true;
+					console.log(num);
+				} else {
+					console.log("not in zoo");
+					for(var i=0; i<image.length; i++) {
+						if (image[i] === srcEle) {
+							console.log(i);
+							var num = i;
+						}
+					}
+				}
 				srcEle.style.cursor = "move";
-				moveFlag = true;
 				eve.preventDefault();
 				srcEle.style.left = dx + "px";
 				srcEle.style.top = dy + "px";
-				relaPosX = mouseX - offsetX - gameZonePos.x + 71;
-				relaPosY = mouseY - offsetY - gameZonePos.y + 41;
-				var num = (Math.ceil(relaPosY/80)-1)*5 + Math.floor(relaPosX/140);
 				if (temp !== num) {
 					li[temp].style.opacity = 1;
 					li[temp].style.border = "";
@@ -199,13 +235,17 @@ window.onload = function () {
 					li[num].style.border = "1px solid red";
 					temp = num;
 				}
+				// }
 			}
 			addEventHandler(ul, "mousemove", handleMousemove);
 			// mouse up
-			var handleMouseup = function () {
+			var handleMouseup = function (eve) {
+				console.log("mouseup");
+				// 防止交换动画时触发mousedown 和 mousemove
+				removeEventHandler(ul, "mousedown", handleMousedown);
+				removeEventHandler(ul, "mousemove", handleMousemove);
+				console.log("remove mousemove");
 				if (moveFlag == true) {
-					removeEventHandler(ul, "mousedown", handleMousedown);
-					removeEventHandler(ul, "mousemove", handleMousemove);
 					srcEle.style.cursor = "";
 					var posX  = temp%5*142 + 71;
 					var posY  = Math.floor(temp/5)*82 + 41;
@@ -222,6 +262,7 @@ window.onload = function () {
 							var img = image[temp];
 							var src = srcEle;
 							var liSrc = srcEle.parentNode;
+							// 把样式初始化(left top opactiy border ...)
 							initial(src);
 							initial(img);
 							if (img !== src) {
@@ -236,21 +277,24 @@ window.onload = function () {
 							if (isSuccess(index)) {
 								alert("you win");
 							} else {
+								// removeEventHandler(ul, "mouseup", handleMouseup);
+								// 交换动画完成后恢复mousedown事件
 								addEventHandler(ul, "mousedown", handleMousedown);
-								removeEventHandler(ul, "mouseup", handleMouseup);
 							}
+
 						}
-					}, 100);	
+					}, 100);
+					// 防止交换动画过程中click触发mouseup
+					removeEventHandler(ul, "mouseup", handleMouseup);
 				} else {
 					// 解决chrome 触发mousedown 后一定会触发mouseover的bug
 					removeEventHandler(ul, "mousemove", handleMousemove);
+					addEventHandler(ul, "mousedown", handleMousedown);
 				}
 			}
 			addEventHandler(ul, "mouseup", handleMouseup);	
 		}
 		addEventHandler(ul, "mousedown", handleMousedown);
-		addEventHandler(ul, "click", function(){});
-		
-
+	
 	}
 }
