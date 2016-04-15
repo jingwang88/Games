295171d88380fc8a34b7230d2fdaf8117653a00f
jingwang88 <wj154419@163.com>
2016-04-16 00:20:03 +0800

    完成放烟花游戏

--- game-two/index.js
+++ game-two/index.js
@@ -1,26 +1,29 @@
 var baseFunc = {
 	addHander: function (obj, type, handler) {
-		return obj.addEventListener ? obj.addEventListener(type, handler) : obj.attachEvent("on"+type, handler);
-	}
+		obj.addEventListener ? obj.addEventListener(type, handler) : obj.attachEvent("on"+type, handler);
+	},
 	removeHander: function (obj, type, handler) {
-		return obj.removeEventListener ? obj.removeEventListener(type, handler) : obj.detachEvent("on"+type, handler);		
-	}
+		obj.removeEventListener ? obj.removeEventListener(type, handler) : obj.detachEvent("on"+type, handler);		
+	},
+	stopProppagation: function (event) {
+		event.stopProppagation ? event.stopProppagation() : event.cancelBubble = true;
+	},
 	getRandomNum: function (min, max) {
-		return Math.random()*(max-min) + min;
-	}
+		return parseInt(Math.random()*(max-min) + min);
+	},
 	getRandomColor: function () {
-		var num = getRandomNum(0, 0xffffff).toString(16);
+		var num = this.getRandomNum(0, 0xffffff).toString(16);
 		return "#" + num;
 	}
 }
 
 var Firework = function () {
-
+	// this.initialize();
 };
 Firework.prototype = {
-	initialize: function (options) {
-		this.fireworkLeft = options.fireworkLeft;
-		this.fireworkTop = options.fireworkTop;
+	initialize: function () {
+		this.fireworkLeft = 0;
+		this.fireworkTop = 0;
 	},
 	manul: function (event) {
 		var event = event || window.event;
@@ -29,59 +32,110 @@ Firework.prototype = {
 		this.create(this.fireworkLeft, this.fireworkTop);
 	},
 	auto: function () {
-		setInterVal(function () {
-			this.fireworkTop = getRandomNum(100, document.documentElement.clientHeight-100);
-			this.fireworkLeft = getRandomNum(100, document.documentElement.clientWidth-100)
-			this.create(this.fireworkLeft, this.fireworkTop);
-		}, 1000);
+		var that = this;
+		var autoTimer = setInterval(function () {
+			this.fireworkTop = baseFunc.getRandomNum(100, document.documentElement.clientHeight-100);
+			this.fireworkLeft = baseFunc.getRandomNum(100, document.documentElement.clientWidth-100);
+			that.create(this.fireworkLeft, this.fireworkTop);	
+		}, 1500);
+		return autoTimer;
 	},
 	// 烟花上升动画
 	create: function (paramX, paramY) {
 		var upFW = document.createElement("div");
+		var boomFW = this.boomFW;
 		// 烟花爆炸位置
-		var top = document.documentElement.clientHeight;
+		var clientHeight = window.innerHeight;
 		with(upFW.style) {
-			positon = "absolute";
+			position = "absolute";
 			left = paramX + "px";
+			top = clientHeight + "px";
 			width = "4px";
-			height = "45px";
+			height = "30px";
 			borderRadius = "2px";
-			backgroundColor = getRandomColor();
+			backgroundColor = baseFunc.getRandomColor();
 		}
 		document.body.appendChild(upFW);
 		var upTimer = setInterval(function () {
-			if (upFW.style.top === paramY + "px") {
+			if (clientHeight < paramY) {
 				document.body.removeChild(upFW);
+				boomFW(paramX, paramY);
 				clearInterval(upTimer);
 			} else {
-				top -= 10;
-				upFW.style.top = top + "px";
+				clientHeight -= 10;
+				upFW.style.top = clientHeight + "px";
 			}
-		}, 20);
+		}, 10);
 	},
-	// 烟花爆竹动画
+	// 烟花爆炸动画
 	boomFW: function (paramX, paramY) {
 		var len;
-		var fwChips = [];
-		navigator.userAgent.indexof("MSIE")>0 ? len=getRandomNum(20, 30) : len=getRandomNum(40, 50);
+		var fwChips = [], speedX = [], speedY = [];
+		navigator.userAgent.indexOf("MSIE")>0 ? len=baseFunc.getRandomNum(20, 30) : len=baseFunc.getRandomNum(40, 50);
 		for (var i=0; i<len; i++) {
 			var chip = document.createElement("div");
-			var speedX = getRandomNum(-20, 20);
-			var speedY = getRandomNum(-20, 20);
+			speedX.push(baseFunc.getRandomNum(-20, 20));
+			speedY.push(baseFunc.getRandomNum(-20, 20));
 			with (chip.style) {
+				position = "absolute";
 				width = "4px";
 				height = "4px";
+				left = paramX + speedX[i] + "px";
+				top = paramY  + speedY[i] + "px";
 				borderRadius = "2px";
-				backgroundColor = getRandomColor();
+				backgroundColor = baseFunc.getRandomColor();
 			}
-			chip.offsetLeft = paramX + speedX();
-			chip.offsetTop = paramY + speedY();
-			fwChips.push(chip);
+			document.body.appendChild(chip);
+			fwChips[i] = chip;
 		}
-		var timer = setInterval(function () {
-			
-		}, 20)
+		var moveTimer = setInterval(function () {
+			for (var j=0; j<fwChips.length; j++) {
+				fwChips[j].style.left = fwChips[j].offsetLeft + speedX[j] + "px";
+				fwChips[j].style.top = fwChips[j].offsetTop + speedY[j] + "px";
+				// 加速下降或者减速上升
+				speedY[j]++;
+				(fwChips[j].offsetLeft < 0 || fwChips[j].offsetLeft > window.innerWidth ||
+				fwChips[j].offsetTop < 0 || fwChips[j].offsetTop > window.innerHeight) && document.body.removeChild(fwChips[j]);
+			}
+		}, 30);
+	}
 }
-window.onload = function () {
-
+window.onload = function() {
+	var manulBtn = document.getElementById("manulFW");
+	var autoBtn = document.getElementById("autoFW");
+	var stopBtn = document.getElementById("stopFW");
+	var body = document.querySelector("body");
+	var firework = new Firework();
+	var manulFlag = true, autoFlag = true;
+	var autoTimer;
+	body.style.height = window.innerHeight + "px";
+	body.style.width = window.innerWidth + "px";
+	var manulHandler = function (event) {
+							firework.manul(event);
+						};
+	manulBtn.onclick = function (e) {
+		baseFunc.stopProppagation(e);
+		clearInterval(autoTimer);
+		autoFlag = true;
+		if (manulFlag) {
+			manulFlag = false;
+			baseFunc.addHander(body, "click", manulHandler);
+		} 
+	};
+	autoBtn.onclick = function (e) {
+		baseFunc.stopProppagation(e);
+		manulFlag = true;
+		if (autoFlag) {
+			autoTimer = firework.auto();
+			autoFlag = false;
+		}
+		baseFunc.removeHander(body, "click", manulHandler);
+	}
+	stopBtn.onclick = function (e) {
+		baseFunc.stopProppagation(e);
+		manulFlag = true;
+		autoFlag = true;
+		clearInterval(autoTimer);
+		baseFunc.removeHander(body, "click", manulHandler);
+	}
 }
